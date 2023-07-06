56789a450c5c51c8ce0e6f4dd643250fed6e2003
Gareth-Moore <gccsmoore3@gmail.com>
2023-07-06 07:39:42 +0100

    Responsive layout for main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const customTheme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "white",
+      },
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={customTheme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
