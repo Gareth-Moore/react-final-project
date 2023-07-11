5f786ce66266e0c5c9ee9b6103452d1456a79143
Gareth-Moore <gccsmoore3@gmail.com>
2023-07-11 16:18:29 +0100

    handle games with no image

--- src/services/crop-image.ts
+++ src/services/crop-image.ts
@@ -1,5 +1,7 @@
+import placeholderImage from "../../public/no-image-placeholder.webp";
+
 function getCroppedImage(image_URL: string) {
-  if (!image_URL) return "";
+  if (!image_URL) return placeholderImage;
   const target = "media/";
   const index = image_URL.indexOf(target) + target.length;
   return image_URL.slice(0, index) + "crop/600/400/" + image_URL.slice(index);
