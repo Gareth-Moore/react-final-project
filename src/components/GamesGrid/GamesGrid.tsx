98e224963a9d0c9eb2799eac7a63ae411c6e6777
Gareth-Moore <gccsmoore3@gmail.com>
2023-07-11 19:22:43 +0100

    refactored games grid

--- src/components/GamesGrid/GamesGrid.tsx
+++ src/components/GamesGrid/GamesGrid.tsx
@@ -13,30 +13,30 @@ const GamesGrid = ({ gameQuery }: Props) => {
   const { games, errors, isLoading } = useFetchGames(gameQuery);
 
   const gameCardSkeletonKeys = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  if (errors) return <Text>{errors}</Text>;
+
   return (
-    <>
-      {errors && <Text>{errors}</Text>}
-      <SimpleGrid
-        columns={{ base: 1, md: 2, lg: 3 }}
-        maxWidth={{ base: 350, md: 720, lg: 1090 }}
-        spacing={"20px"}
-        mx={"auto"}
-        justifyContent={"center"}
-      >
-        {isLoading &&
-          gameCardSkeletonKeys.map((value) => (
-            <GameCardContainer key={value}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {games &&
-          games.map((game) => (
-            <GameCardContainer key={game.id}>
-              <GameCard game={game}></GameCard>
-            </GameCardContainer>
-          ))}
-      </SimpleGrid>
-    </>
+    <SimpleGrid
+      columns={{ base: 1, md: 2, lg: 3 }}
+      maxWidth={{ base: 350, md: 720, lg: 1090 }}
+      spacing={"20px"}
+      mx={"auto"}
+      justifyContent={"center"}
+    >
+      {isLoading &&
+        gameCardSkeletonKeys.map((value) => (
+          <GameCardContainer key={value}>
+            <GameCardSkeleton />
+          </GameCardContainer>
+        ))}
+      {games &&
+        games.map((game) => (
+          <GameCardContainer key={game.id}>
+            <GameCard game={game}></GameCard>
+          </GameCardContainer>
+        ))}
+    </SimpleGrid>
   );
 };
 
